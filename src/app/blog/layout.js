17a6c40351c77735b655e8cabacc9e820e97f8bc
unknown <nurhidayat.adi01@gmail.com>
2023-12-30 13:02:01 +0700

    working on navbar

---
+++ src/app/blog/layout.js
@@ -0,0 +1,10 @@
+const BlogPage = ({children}) => {
+    return (
+        <div>
+            <h2>This is the blog layout</h2>
+            {children}
+        </div>
+    )
+}
+
+export default BlogPage
