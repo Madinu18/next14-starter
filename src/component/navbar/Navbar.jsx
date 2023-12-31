615f9f29d00cf46678fbbf16d13c745f6aa92146
unknown <nurhidayat.adi01@gmail.com>
2023-12-31 11:00:05 +0700

    Home page complated

--- src/component/navbar/Navbar.jsx
+++ src/component/navbar/Navbar.jsx
@@ -1,10 +1,12 @@
+import Link from "next/link"
 import Links from "./links/links"
 import styles from  "./navbar.module.css"
 
 const Navbar = () => {
+    
     return (
         <div className={styles.container}>
-            <div className={styles.logo}>Logo</div>
+            <Link href="/" className={styles.logo}>Lama</Link>
             <div>
                 <Links/>
             </div>
